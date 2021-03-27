7ca02d0333a9b597dc79468d7a3d36b96cd7c543
allzone98 <80627036+allzone98@users.noreply.github.com>
2021-03-27 13:15:56 +0700

    Update safelinkutama.js

--- safelinkutama.js
+++ safelinkutama.js
@@ -24,7 +24,8 @@ var xxx=null;
 if(x=="1"){xxx="09/practice-environmental-qwareness-to-our_27.html"}
 if(x=="2"){xxx="09/advantages-of-being-home-business-owner_27.html"}
 if(x=="3"){xxx="09/10-tips-for-start-small-business-startup_27.html"}
-if(x=="4"){xxx="09/the-way-to-start-business-at-home_27.html"}a_to_ve[i].href="https://safelinkkhu.blogspot.com/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="09/the-way-to-start-business-at-home_27.html"}
+if(x=="5"){xxx="03/how-adsense-changed-internet.html"}a_to_ve[i].href="https://safelinkkhu.blogspot.com/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
